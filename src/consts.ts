34030c284cf364c8f98a0db46fb351fba207e531
Sunil K S Mayya <sunilksmayya@Sunils-MacBook-Pro.local>
2026-06-28 14:02:49 +0200

    Initial site: Astro blog, About page, Netlify config

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sunil Mayya';
+export const SITE_DESCRIPTION = 'Firefox engineer at Mozilla, writing about browser internals, web standards, and life outside the editor.';
